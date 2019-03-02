fbe3108adeca363817f43e98cd19a3c7394e698b
irfan <irfan126@gmail.com>
2019-03-02 15:44:34 +0000

    Adds endpoints for creation of rentals and search rental by city

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Rental = require('../models/rental');
+const User = require('../models/user');
+const { normalizeErrors } = require('../helpers/mongoose');
 
 const UserCtrl = require('../controllers/user');
 
 router.get('/secret', UserCtrl.authMiddleware, function(req, res){
 	res.json({"secret": true});
-});
-
-router.get('', function(req, res) {
-	Rental.find({})
-		  .select('-bookings')
-		  .exec( function(err, foundRentals) {
-		res.json(foundRentals);
-	});
 });
 
 router.get('/:id', function(req,res) {
@@ -28,6 +22,45 @@ router.get('/:id', function(req,res) {
 				}
 				return res.json(foundRental);
 			});
+});
+
+//Adding a rental advert to DB
+router.post('', UserCtrl.authMiddleware, function(req,res){
+	const {title, city, street, category, image, shared, bedroom, description, dailyRate } = req.body;
+	const user = res.locals.user;
+
+	const rental = new Rental({title, city, street, category, image, shared, bedroom, description, dailyRate});
+	rental.user = user; //adding USER to rental object
+
+	//Adding new Rental to RENTAL DB
+	Rental.create(rental, function(err, newRental){
+			if (err) {
+				  	  return res.status(422).send({errors: normalizeErrors(err.errors)});
+				  	  }
+			//Updating the user in the USER DB with new Rental ID 	  	  
+			User.update({_id: user.id}, {$push: {rentals: newRental}}, function(){});
+
+			return res.json(newRental);
+	});
+});
+
+//Getting all rentals from DB or Searched City Rentals for rentals listing at frontend
+router.get('', function(req, res) {
+
+	const city = req.query.city;
+	const query = city ? {city: city.toLowerCase()} : {};
+
+	Rental.find(query)
+		  .select('-bookings')
+		  .exec(function(err, foundRentals) {
+			if (err) {
+				  	  return res.status(422).send({errors: normalizeErrors(err.errors)});
+				  	  }
+			if (city && foundRentals.length === 0) {
+				  	  return res.status(422).send({errors: [{title: 'No rentals found!', detail:`There are no rentals for ${city}`}]});
+				  	 }
+					return res.json(foundRentals);
+		});
 });
 
 module.exports = router;
